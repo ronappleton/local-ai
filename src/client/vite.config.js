024cfbe2e5a308dbe2d7e37f2507e6af1eebb48d
Ron Appleton <ron.appleton@gmail.com>
2025-07-11 20:34:55 +0100

    fix: include password reset pages in build

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reset: resolve(__dirname, 'reset/index.html'),
+        request: resolve(__dirname, 'reset/request/index.html')
+      }
     }
   }
 })
