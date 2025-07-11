bb4f19e157c56da9756c58761fba04809a518a46
Ron Appleton <ron.appleton@gmail.com>
2025-07-11 10:23:20 +0100

    Refactor client to Vue layout

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <h1>LocalAI UI</h1>
-  <p>Edit <code>src/App.vue</code> to get started.</p>
+  <ChatLayout />
 </template>
 
-<script setup>
-// Add your composition API code here
+<script setup lang="ts">
+import ChatLayout from './components/ChatLayout.vue'
 </script>
 
 <style>
-h1 {
-  margin: 0;
-  padding: 1rem;
-  text-align: center;
-}
 </style>
